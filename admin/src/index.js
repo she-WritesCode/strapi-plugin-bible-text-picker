8519624619fc4910c8265858909a1f9466d88e72
Busola Okeowo <busolaokemoney@gmail.com>
2022-12-30 13:04:26 +0100

    Added BibleTextDropdown Component

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,63 +1,82 @@
-import { prefixPluginTranslations } from '@strapi/helper-plugin';
-import pluginPkg from '../../package.json';
-import pluginId from './pluginId';
-import Initializer from './components/Initializer';
-import PluginIcon from './components/PluginIcon';
+import { prefixPluginTranslations } from "@strapi/helper-plugin";
+import pluginPkg from "../../package.json";
+import pluginId from "./pluginId";
+import Initializer from "./components/Initializer";
+import PluginIcon from "./components/PluginIcon";
 
 const name = pluginPkg.strapi.name;
 
 export default {
-  register(app) {
-    app.addMenuLink({
-      to: `/plugins/${pluginId}`,
-      icon: PluginIcon,
-      intlLabel: {
-        id: `${pluginId}.plugin.name`,
-        defaultMessage: name,
-      },
-      Component: async () => {
-        const component = await import(/* webpackChunkName: "[request]" */ './pages/App');
+	register(app) {
+		app.addMenuLink({
+			to: `/plugins/${pluginId}`,
+			icon: PluginIcon,
+			intlLabel: {
+				id: `${pluginId}.plugin.name`,
+				defaultMessage: name,
+			},
+			Component: async () => {
+				const component = await import(/* webpackChunkName: "[request]" */ "./pages/App");
 
-        return component;
-      },
-      permissions: [
-        // Uncomment to set the permissions of the plugin here
-        // {
-        //   action: '', // the action name should be plugin::plugin-name.actionType
-        //   subject: null,
-        // },
-      ],
-    });
-    app.registerPlugin({
-      id: pluginId,
-      initializer: Initializer,
-      isReady: false,
-      name,
-    });
-  },
+				return component;
+			},
+			permissions: [
+				// Uncomment to set the permissions of the plugin here
+				// {
+				//   action: '', // the action name should be plugin::plugin-name.actionType
+				//   subject: null,
+				// },
+			],
+		});
+		app.registerPlugin({
+			id: pluginId,
+			initializer: Initializer,
+			isReady: false,
+			name,
+		});
 
-  bootstrap(app) {},
-  async registerTrads({ locales }) {
-    const importedTrads = await Promise.all(
-      locales.map((locale) => {
-        return import(
-          /* webpackChunkName: "translation-[request]" */ `./translations/${locale}.json`
-        )
-          .then(({ default: data }) => {
-            return {
-              data: prefixPluginTranslations(data, pluginId),
-              locale,
-            };
-          })
-          .catch(() => {
-            return {
-              data: {},
-              locale,
-            };
-          });
-      })
-    );
+		app.customFields.register({
+			name: "bible-text-dropdown",
+			pluginId: pluginId, // the custom field is created by a color-picker plugin
+			type: "string", // the color will be stored as a string
+			intlLabel: {
+				id: `${pluginId}.bible-text-dropdown.label`,
+				defaultMessage: "Bible Verse",
+			},
+			intlDescription: {
+				id: `${pluginId}.bible-text-dropdown.description`,
+				defaultMessage: "Enter a valid Bible verse",
+			},
+			// icon: ColorPickerIcon, // don't forget to create/import your icon component
+			components: {
+				Input: async () => import(/* webpackChunkName: "input-component" */ "./components/BibleTextPicker"),
+			},
+			options: {
+				// declare options here
+			},
+		});
+	},
 
-    return Promise.resolve(importedTrads);
-  },
+	bootstrap(app) {},
+	async registerTrads({ locales }) {
+		const importedTrads = await Promise.all(
+			locales.map((locale) => {
+				return import(/* webpackChunkName: "translation-[request]" */ `./translations/${locale}.json`)
+					.then(({ default: data }) => {
+						return {
+							data: prefixPluginTranslations(data, pluginId),
+							locale,
+						};
+					})
+					.catch(() => {
+						return {
+							data: {},
+							locale,
+						};
+					});
+			}),
+		);
+
+		return Promise.resolve(importedTrads);
+	},
 };
